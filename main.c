240007311e031ae4153e1d44242e3b0746fa8e06
unyeo32 <unyeo32@sm.ac.kr>
2020-11-05 16:07:23 +0900

    exercise4

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	int i=10;
-	char c='a';
-	
-	int *iptr  =&i;
-	char *cptr =&c;
-	int *iptr2=iptr;
-	
-	printf("i: %p\n %p (size:%i)\n", iptr, &i, sizeof(iptr));
-	printf("c: %p\n %p (size:%i)\n", cptr, &c, sizeof(cptr));
-	printf("iptr2: %p, %i\n", iptr2, *iptr2);
-	
+void swap (int *x, int *y){
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+}
+void main(void) {
+	int a=3;
+	int b=5;
+	swap (&a,&b);
+	printf("a: %i, b:%i\n", a,b);
 	return 0;
 }
+
+
